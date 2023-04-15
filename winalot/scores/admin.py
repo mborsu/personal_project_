23e0ec6f5a25ae0c0e9d7355bdf1187f5a32ba96
Marius Borsu Lefevre <marius.borsu-lefevree@isae-supaero.fr>
2023-04-15 12:53:38 +0200

    Fixed circular issues with the models

--- winalot/scores/admin.py
+++ winalot/scores/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Team, Tournament
+from .models import Team, Tournament, Pool, Match, Player
 
 admin.site.register(Tournament)
 
 admin.site.register(Team)
+
+admin.site.register(Pool)
+
+admin.site.register(Match)
+
+admin.site.register(Player)
--- winalot/scores/models.py
+++ winalot/scores/models.py
@@ -1,24 +1,21 @@
 from django.db import models
 
-
 class Tournament(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255, blank=True)
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
-    teams = models.ManyToManyField('Team', related_name='tournaments')
     num_pools = models.PositiveIntegerField()
     num_teams_per_pool = models.PositiveIntegerField()
 
 class Pool(models.Model):
     number = models.PositiveIntegerField()
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='pools')
-    teams = models.ManyToManyField('Team', related_name='pools')
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
     coach_name = models.CharField(max_length=255)
-    members = models.ManyToManyField('Player', related_name='teams')
+    pool = models.ForeignKey(Pool, on_delete=models.CASCADE, related_name='teams')
 
 class Match(models.Model):
     date_time = models.DateTimeField()
@@ -27,7 +24,6 @@ class Match(models.Model):
     away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
     home_score = models.PositiveIntegerField(null=True, blank=True)
     away_score = models.PositiveIntegerField(null=True, blank=True)
-    pool = models.ForeignKey(Pool, on_delete=models.CASCADE, related_name='matches')
 
 class Player(models.Model):
     name = models.CharField(max_length=255)
