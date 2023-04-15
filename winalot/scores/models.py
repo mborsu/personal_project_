e395b89e4df51ea130f00d4ff705d63ed066c6b3
Marius Borsu Lefevre <marius.borsu-lefevree@isae-supaero.fr>
2023-04-15 13:04:31 +0200

    fixed displaying issues in admin panel, not showing names

--- winalot/scores/models.py
+++ winalot/scores/models.py
@@ -7,15 +7,22 @@ class Tournament(models.Model):
     end_date = models.DateTimeField(null=True, blank=True)
     num_pools = models.PositiveIntegerField()
     num_teams_per_pool = models.PositiveIntegerField()
+    def __str__(self):
+        return self.name
 
 class Pool(models.Model):
     number = models.PositiveIntegerField()
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='pools')
+    def __str__(self):
+        return self.number
+
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
     coach_name = models.CharField(max_length=255)
     pool = models.ForeignKey(Pool, on_delete=models.CASCADE, related_name='teams')
+    def __str__(self):
+        return self.name
 
 class Match(models.Model):
     date_time = models.DateTimeField()
@@ -24,7 +31,11 @@ class Match(models.Model):
     away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
     home_score = models.PositiveIntegerField(null=True, blank=True)
     away_score = models.PositiveIntegerField(null=True, blank=True)
+    def __str__(self):
+        return self.id
 
 class Player(models.Model):
     name = models.CharField(max_length=255)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
+    def __str__(self):
+        return self.name
