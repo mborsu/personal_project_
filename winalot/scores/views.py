726b1c8be0c5be115252e911cb23c7dbcc919e23
Marius Borsu Lefevre <marius.borsu-lefevree@isae-supaero.fr>
2023-04-19 12:14:34 +0200

    Made fancier HTML, added return buttons everywhere

--- winalot/scores/views.py
+++ winalot/scores/views.py
@@ -3,7 +3,6 @@ from .models import Pool, Tournament, Match
 from django.views import generic
 
 
-
 class IndexView(generic.ListView):
     template_name = 'scores/index.html'
     model = Tournament
@@ -15,20 +14,20 @@ class IndexView(generic.ListView):
         print (f"tournaments={tournaments}")
         return tournaments
     
-
-
+    
 class PoolView(generic.DetailView):
     template_name = 'scores/detail.html'
     
     def get_queryset(self):
         return Tournament.objects.order_by('name')
-        
+
 
 class MatchView(generic.DetailView):
     template_name = 'scores/match_detail.html'
     
     def get_queryset(self):
         return Pool.objects.order_by('name')
+    
     
 class CommentView(generic.DetailView):
     template_name = 'scores/match_comment.html'
